82ae6b713ea2219b8a122387e0e44f707eeeeec7
Rich Swartzwelder <rich680@gmail.com>
2019-01-10 22:22:32 -0500

    Updates

---
+++ precache-manifest.dc26f59dbe49e9d408b43a924b91f2da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ded0a802bf316c8b01f3",
+    "url": "/react-todo-list/static/css/main.8950c1a3.chunk.css"
+  },
+  {
+    "revision": "ded0a802bf316c8b01f3",
+    "url": "/react-todo-list/static/js/main.ded0a802.chunk.js"
+  },
+  {
+    "revision": "fc99ba1120b9785b5931",
+    "url": "/react-todo-list/static/js/1.fc99ba11.chunk.js"
+  },
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "68cda9bbe689cdb8e5ebf9ae5b5ac540",
+    "url": "/react-todo-list/index.html"
+  }
+];
